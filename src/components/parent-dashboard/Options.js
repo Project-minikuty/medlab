8c85163869613cdc050c145fb2faa4f31fe9880d
Akash Vijay <akashvijay@ieee.org>
2023-05-22 12:56:48 +0530

    [Update] Added Options in Parent Dashboard

--- src/components/parent-dashboard/Options.js
+++ src/components/parent-dashboard/Options.js
@@ -10,35 +10,55 @@ function ButtonGrid() {
   const buttonStyle = {
     borderColor: '#498589',
     color: '#498589',
-    margin: '5px',
-    padding: '5px 10px',
-    borderRadius: '4px',
-    background: 'none',
+    padding: '10px 30px',
+    borderRadius: '25px',
+    background: 'white',
     fontWeight: 'bold',
     cursor: 'pointer',
+    width: '400px',
+    marginBottom: '25px',
+    marginLeft: '8px',
   };
 
+  const desktopButtonStyle = {
+    ...buttonStyle,
+    marginLeft: '190px',
+  };
+
+  const isDesktopView = window.innerWidth > 768; // Adjust the breakpoint as per your design
+
   return (
-    <div >
-   <Heading />
-    <SubHeading />
     <div>
-      <button style={buttonStyle} onClick={() => handleButtonClick(1)}>Button 1</button>
+      <Heading />
+      <SubHeading />
+      <br />
+      <br />
+      <div>
+        <button style={isDesktopView ? desktopButtonStyle : buttonStyle} onClick={() => handleButtonClick(1)}>
+          <img src={require('../../images/parentOptions/option1.png')} height="15px" width="17px" alt="option1"></img>&nbsp; View Student Progress
+        </button>
+      </div>
+      <div>
+        <button style={isDesktopView ? desktopButtonStyle : buttonStyle} onClick={() => handleButtonClick(2)}><img src={require('../../images/parentOptions/option2.png')} height="17px" width="17px" alt="option2"></img>&nbsp;
+          View Student Profile
+        </button>
+      </div>
+      <div>
+        <button style={isDesktopView ? desktopButtonStyle : buttonStyle} onClick={() => handleButtonClick(3)}><img src={require('../../images/parentOptions/option3.png')} height="17px" width="19px" alt="option3"></img>&nbsp;
+          View Assignments
+        </button>
+      </div>
+      <div>
+        <button style={isDesktopView ? desktopButtonStyle : buttonStyle} onClick={() => handleButtonClick(4)}><img src={require('../../images/parentOptions/option4.png')} height="17px" width="19px" alt="option4"></img>&nbsp;
+          Submit Assignment
+        </button>
+      </div>
+      <div>
+        <button style={isDesktopView ? desktopButtonStyle : buttonStyle} onClick={() => handleButtonClick(5)}><img src={require('../../images/parentOptions/option5.png')} height="17px" width="17px" alt="option1"></img>&nbsp;
+          Schedule Appointments
+        </button>
+      </div>
     </div>
-    <div>
-    <button style={buttonStyle} onClick={() => handleButtonClick(2)}>Button 1</button>
-  </div>
-  <div>
-    <button style={buttonStyle} onClick={() => handleButtonClick(3)}>Button 1</button>
-  </div>
-  <div>
-    <button style={buttonStyle} onClick={() => handleButtonClick(4)}>Button 1</button>
-  </div>
-  <div>
-    <button style={buttonStyle} onClick={() => handleButtonClick(5)}>Button 1</button>
-  </div>
-  </div>
-    
   );
 }
 
