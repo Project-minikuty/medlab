a21b9de6032213530abd774d9f323c6331f78c95
Akash Vijay <akashvijay@ieee.org>
2023-05-27 17:30:08 +0530

    [Update] Changed Admin Dashboard Layout

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from 'react';
 
-function ButtonGrid({ buttons }) {
+function ButtonGrid({ buttons, isHighlighted }) {
   const handleButtonClick = (buttonId) => {
     console.log(`Button ${buttonId} clicked`);
   };
 
   const buttonStyle = {
     borderColor: '#498589',
-    color: '#498589',
-    padding: '10px 30px',
+    color: isHighlighted ? '#F5FDFB':'#498589',
+    padding: isHighlighted ? '40px 50px' : '10px 30px',
     borderRadius: '25px',
-    background: 'white',
+    background: isHighlighted ? '#10C99D' : 'white',
     fontWeight: 'bold',
     cursor: 'pointer',
-    width: '390px',
+    width: isHighlighted ? '160px' : '390px',
     marginBottom: '25px',
     marginLeft: '10px',
     marginRight: 'auto',
@@ -34,30 +34,57 @@ function ButtonGrid({ buttons }) {
 
     handleResize();
 
+    const handleOrientationChange = () => {
+      setIsDesktopView(window.innerWidth > 768);
+    };
+
     window.addEventListener('resize', handleResize);
+    window.addEventListener('orientationchange', handleOrientationChange);
 
     return () => {
       window.removeEventListener('resize', handleResize);
+      window.removeEventListener('orientationchange', handleOrientationChange);
     };
   }, []);
 
-  useEffect(() => {
-    document.body.style.overflowX = isDesktopView ? 'auto' : 'hidden';
-  }, [isDesktopView]);
+  const renderButtons = () => {
+    if (isDesktopView && isHighlighted) {
+      const rows = [];
+      for (let i = 0; i < buttons.length; i += 2) {
+        const button1 = buttons[i];
+        const button2 = buttons[i + 1];
+        rows.push(
+          <div key={i} style={{ display: 'flex', justifyContent: 'center' }}>
+            <button style={desktopButtonStyle} onClick={() => handleButtonClick(i)}>
+              <img src={button1.img} height="15px" width="17px" alt={button1.alt} />&nbsp; {button1.text}
+            </button>
+            {button2 && (
+              <button style={desktopButtonStyle} onClick={() => handleButtonClick(i + 1)}>
+                <img src={button2.img} height="15px" width="17px" alt={button2.alt} />&nbsp; {button2.text}
+              </button>
+            )}
+          </div>
+        );
+      }
+      return rows;
+    } else {
+      return buttons.map((button, index) => (
+        <div key={index}>
+          <button style={buttonStyle} onClick={() => handleButtonClick(index)}>
+            <img src={button.img} height="15px" width="17px" alt={button.alt} />&nbsp; {button.text}
+          </button>
+        </div>
+      ));
+    }
+  };
+  
 
   return (
     <>
       <div>
-      
         <br />
         <br />
-        {buttons.map((button, index) => (
-          <div key={index}>
-            <button style={isDesktopView ? desktopButtonStyle : buttonStyle} onClick={() => handleButtonClick(index)}>
-              <img src={button.img} height="15px" width="17px" alt={button.alt} />&nbsp; {button.text}
-            </button>
-          </div>
-        ))}
+        {renderButtons()}
       </div>
     </>
   );
--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import BrandNav from "../components/BrandNav";
 import "./dashboard.css";
 import Heading from "../components/Heading";
-
 import Bg from "../components/Bg";
 import ButtonGrid from "../components/Options";
 
@@ -14,20 +13,32 @@ function Doctor() {
     { img: require('../images/adminOptions/option3.png'), alt: 'option3', text: 'Add New User' },
     { img: require('../images/adminOptions/option4.png'), alt: 'option4', text: 'Delete User' },
   ];
-  
+
+  const [forceRender, setForceRender] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setForceRender(!forceRender);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [forceRender]);
+
+  const isHighlighted = window.innerWidth > 768;
 
   return (
-   <>
-      <BrandNav logout="true"/>
-      <Heading view = "desktop" type={1}/>
+    <>
+      <BrandNav logout="true" />
+      <Heading view="desktop" type={1} />
       <div className="parentcontainer">
-
-      <ButtonGrid buttons={buttons} />
-
-        <Bg type={1}/>
+        <ButtonGrid buttons={buttons} isHighlighted={isHighlighted} />
+        <Bg type={1} />
       </div>
-   
-   </>
+    </>
   );
 }
 
