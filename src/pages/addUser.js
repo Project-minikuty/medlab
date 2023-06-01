d7d399a83f7c1d892837547e4bf38ad867f79ad8
Akash Vijay <akashvijay@ieee.org>
2023-06-01 20:44:15 +0530

    [Update] Dynamic Form Rendering

--- src/pages/addUser.js
+++ src/pages/addUser.js
@@ -1,67 +1,109 @@
 import "./addUser.css";
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/BrandNav";
 import Heading from "../components/Heading";
-import { Dropdown } from "bootstrap";
+
 
 export default function App() {
+  const [userRole, setUserRole] = useState("parent");
+
+  const handleRoleChange = (e) => {
+    setUserRole(e.target.value);
+  };
+
   return (
-    <div className=".App">
+    <div className="App">
       <Navbar logout="true" />
       <div className="body1">
-        {/* <h2 style='{{marginRight:"20px"}}>Add new user</h2>  */}
         <div className="title">
           <Heading text="Add new user" />
         </div>
         <form>
           <div className="wrapper1">
-            <div className="d-flex">
-              <div className="d-flex justify-content-between">
-                <label>Name:</label>
-                <input type="text" name="name" />
-              </div>
-              <div className="d-flex justify-content-between">
-                <label>Date of Birth:</label>
-                <input type="date" name="dob" />
-              </div>
-            </div>
-            <div className="d-flex">
-              <div className="d-flex justify-content-between">
-                <label>Age:</label>
-                <input type="number" name="age" />
-              </div>
-              <div className="d-flex justify-content-between">
-                <label>Blood Group:</label>
-                <input type="text" list="blood" name="bloodgroup" required="true" />
-                <datalist id="blood">
-                  <option value="O+ve" />
-                  <option value="O-ve" />  {/* vere method undo ennu nokkane dropdown allel ingane vekkam */}
-                  <option value="A+ve" />
-                </datalist>
-               
-              </div>
-            </div>
-            <div className="d-flex">
-              <div className="d-flex justify-content-between">
-                <label>Height:</label>
-                <input type="text" name="height" />
-              </div>
-              <div className="d-flex justify-content-between">
-                <label>Weight:</label>
-                <input type="text" name="weight" />
-              </div>
+            <div className="d-flex justify-content-center userRole">
+              <label className="userRoleLabel">User Role:</label>
+              <select
+                value={userRole}
+                onChange={handleRoleChange}
+                name="userRole"
+                className="customDropdown"
+              >
+                <option value="parent">Parent</option>
+                <option value="admin">Admin</option>
+                <option value="doctor">Doctor</option>
+              </select>
             </div>
 
-            <div className="d-flex">
-              <div className="d-flex justify-content-between">
-                <label>Name of Guardian:</label>
-                <input type="text" name="name_of_guardian" />
-              </div>
-              <div className="d-flex justify-content-between">
-                <label>Address:</label>
-                <input type="text" name="address" />
-              </div>
-            </div>
+            {userRole === "parent" && (
+              <>
+                <div className="d-flex">
+                  <div className="d-flex justify-content-between">
+                    <label>Name:</label>
+                    <input type="text" name="name" />
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <label>Date of Birth:</label>
+                    <input type="date" name="dob" />
+                  </div>
+                </div>
+                <div className="d-flex">
+                  <div className="d-flex justify-content-between">
+                    <label>Age:</label>
+                    <input type="number" name="age" />
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <label>Blood Group:</label>
+                    <input
+                      type="text"
+                      list="blood"
+                      name="bloodgroup"
+                      required="true"
+                    />
+                    <datalist id="blood">
+                      <option value="O+ve" />
+                      <option value="O-ve" />
+                      <option value="A+ve" />
+                    </datalist>
+                  </div>
+                </div>
+                <div className="d-flex">
+                  <div className="d-flex justify-content-between">
+                    <label>Height:</label>
+                    <input type="text" name="height" />
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <label>Weight:</label>
+                    <input type="text" name="weight" />
+                  </div>
+                </div>
+                <div className="d-flex">
+                  <div className="d-flex justify-content-between">
+                    <label>Name of Guardian:</label>
+                    <input type="text" name="name_of_guardian" />
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <label>Address:</label>
+                    <input type="text" name="address" />
+                  </div>
+                </div>
+              
+              </>
+            )}
+
+            {userRole !== "parent" && (
+              <>
+                <div className="d-flex">
+                  <div className="d-flex justify-content-between">
+                    <label>Name:</label>
+                    <input type="text" name="name" />
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <label>Date of Birth:</label>
+                    <input type="date" name="dob" />
+                  </div>
+                </div>
+              </>
+            )}
 
             <div className="d-flex">
               <div className="d-flex justify-content-between">
@@ -74,11 +116,10 @@ export default function App() {
               </div>
             </div>
             <div className="d-flex justify-content-center">
-              <input type="submit"></input>
-              <input type="reset"></input>
-              </div>
+              <input type="submit" />
+              <input type="reset" />
+            </div>
           </div>
-         
         </form>
       </div>
     </div>
