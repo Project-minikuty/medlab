46b53795852ff1b5bd750878c7c58e4a823e8cf3
Tomin Joy <tominjk007@gmail.com>
2023-06-01 17:34:04 +0530

    onnum illa randu button vechu

--- src/pages/addUser.js
+++ src/pages/addUser.js
@@ -2,8 +2,7 @@ import "./addUser.css";
 import React from "react";
 import Navbar from "../components/BrandNav";
 import Heading from "../components/Heading";
-
-
+import { Dropdown } from "bootstrap";
 
 export default function App() {
   return (
@@ -12,11 +11,10 @@ export default function App() {
       <div className="body1">
         {/* <h2 style='{{marginRight:"20px"}}>Add new user</h2>  */}
         <div className="title">
-        <Heading text="Add new user" />
+          <Heading text="Add new user" />
         </div>
         <form>
           <div className="wrapper1">
-            
             <div className="d-flex">
               <div className="d-flex justify-content-between">
                 <label>Name:</label>
@@ -34,7 +32,13 @@ export default function App() {
               </div>
               <div className="d-flex justify-content-between">
                 <label>Blood Group:</label>
-                <input type="text" name="bloodgroup" />
+                <input type="text" list="blood" name="bloodgroup" required="true" />
+                <datalist id="blood">
+                  <option value="O+ve" />
+                  <option value="O-ve" />  {/* vere method undo ennu nokkane dropdown allel ingane vekkam */}
+                  <option value="A+ve" />
+                </datalist>
+               
               </div>
             </div>
             <div className="d-flex">
@@ -50,34 +54,31 @@ export default function App() {
 
             <div className="d-flex">
               <div className="d-flex justify-content-between">
-              <label>
-              Name of Guardian:
-              </label>
-              <input type="text" name="name_of_guardian" />
+                <label>Name of Guardian:</label>
+                <input type="text" name="name_of_guardian" />
               </div>
               <div className="d-flex justify-content-between">
-              <label>
-              Address:
-              </label>
-              <input type="text" name="address" />
+                <label>Address:</label>
+                <input type="text" name="address" />
               </div>
             </div>
 
             <div className="d-flex">
               <div className="d-flex justify-content-between">
-              <label>
-              Username:
-              </label>
-              <input type="text" name="username" />
+                <label>Username:</label>
+                <input type="text" name="username" />
               </div>
               <div className="d-flex justify-content-between">
-              <label>
-              Password:
-              </label>
-              <input type="password" name="password" />
+                <label>Password:</label>
+                <input type="password" name="password" />
               </div>
-            </div> 
+            </div>
+            <div className="d-flex justify-content-center">
+              <input type="submit"></input>
+              <input type="reset"></input>
+              </div>
           </div>
+         
         </form>
       </div>
     </div>
