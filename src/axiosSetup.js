83affbbc36f64524b154e4cd71e9eedef2cc08c5
Akash Vijay <akashvijay@ieee.org>
2023-06-07 13:20:00 +0530

    [Update] Axios Setup

--- src/axiosSetup.js
+++ src/axiosSetup.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// export default axios.create({
-//     baseURL: "https://medback.onrender.com/"
-// });
-
 export default axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://medback.onrender.com/"
 });
+
+// export default axios.create({
+//     baseURL: "http://127.0.0.1:8000"
+// });
