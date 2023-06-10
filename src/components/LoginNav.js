7c595949b9b93d2aaa0e8dba04aadf7803e352c3
Akash Vijay <akashvijay@ieee.org>
2023-06-10 22:39:47 +0530

    [Update] Added New SideBar

---
+++ src/components/LoginNav.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./Brandnav.css";
+import { useNavigate } from "react-router";
+
+function BrandNav2(props) {
+  const navigate = useNavigate();
+  const navBrandStyle = {
+    fontWeight: "bold",
+    fontFamily: "Lato, sans-serif",
+  };
+
+  const handleLogout = () => {
+    // Add your logout logic here
+    console.log("Logout clicked");
+    localStorage.clear();
+    navigate("/lo");
+  };
+
+  const buttonStyle = {
+    borderColor: "#498589",
+    color: "#498589",
+    fontWeight: "bold",
+    // Remove the hovering effect
+    "&:hover": {
+      backgroundColor: "transparent",
+      color: "#498589",
+      borderColor: "#498589",
+    },
+  };
+
+  return (
+    <>
+      
+      
+
+     
+      <nav className="navbar navbar-light bg-light d-none d-lg-flex">
+        <div className="container d-flex justify-content-between align-items-center">
+          <a href="#home" className="navbar-brand" style={navBrandStyle}>
+            <img
+              alt="logo"
+              src={require("../images/MedlabLogo.png")}
+              width="35"
+              height="25"
+              className="d-inline-block align-top"
+            />{" "}
+            <span>MEDLAB</span>
+          </a>
+          <div>
+            <a href="#about" className="nav-link about">
+              About
+            </a>
+            <a href="#team" className="nav-link team">
+              Team
+            </a>
+            <a href="#contact" className="nav-link contact">
+              Contact
+            </a>
+          </div>
+          {props.logout ? (
+            <button
+              className="btn btn-outline"
+              onClick={handleLogout}
+              style={buttonStyle}
+            >
+              Logout
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default BrandNav2;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import logo from './logo.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
-import BrandNav from "../components/BrandNav";
+import BrandNav from "../components/LoginNav";
 import Form from "../components/login-page/Login-form";
 import Bg from "../components/login-page/Login-Bg";
 import "./login.css";
