77b1740e81db3403ba09bc0b6f90bea3d522d864
Akash Vijay <akashvijay@ieee.org>
2023-06-10 15:33:11 +0530

    [Update] Added Supending Feature

--- src/components/StudentListView.js
+++ src/components/StudentListView.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 import "./ListView.css";
 
 export default function ListView(props) {
+  const [suspendedUsers, setSuspendedUsers] = useState([]);
   const [canSuspend, setCanSuspend] = useState(true);
 
-
-  const handleSuspend = () => {
-    // Add the logic to handle suspend action here
+  const handleSuspend = async (user) => {
+    const admin_username = localStorage.getItem("username");
+    try {
+      await axios.post(`https://medback.onrender.com/${admin_username}/suspend/${user.username}`);
+      setSuspendedUsers((prevSuspendedUsers) => [...prevSuspendedUsers, user._id]);
+    } catch (error) {
+      console.error("Error suspending user:", error);
+    }
   };
 
   return (
@@ -16,14 +23,12 @@ export default function ListView(props) {
         <div className="d-flex flex-col font-weight-bold">Username</div>
         <div className="d-flex flex-col font-weight-bold">Name</div>
         <div className="d-flex flex-col font-weight-bold">Status</div>
-        {/* <button type="button" className="btn btn-outline-danger">Danger</button> */}
       </div>
-     
 
       {props.List.map((user) => (
         <div
           className={`d-flex flex-row listBody ${
-            !canSuspend ? "red-bg" : ""
+            suspendedUsers.includes(user._id) ? "red-bg" : ""
           }`}
           key={user._id}
         >
@@ -32,8 +37,8 @@ export default function ListView(props) {
           <button
             type="button"
             className="btn btn-outline-danger"
-            onClick={handleSuspend}
-            disabled={!canSuspend}
+            onClick={() => handleSuspend(user)}
+            disabled={!canSuspend || suspendedUsers.includes(user._id)}
           >
             Suspend
           </button>
