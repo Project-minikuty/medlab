ac61369c5dbca488850348e93bcc3cd162f2bd42
Tomin Joy <tominjk007@gmail.com>
2023-06-28 18:49:23 +0530

    chat kit changed

--- src/components/DMScreen.js
+++ src/components/DMScreen.js
@@ -1,39 +1,251 @@
-import React from 'react';
-import './DMScreen.css';
+import styles from "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import avtr from "../images/avtr.png";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  Sidebar,
+  Search,
+  ConversationList,
+  Conversation,
+  Avatar,
+  ConversationHeader,
+  VoiceCallButton,
+  VideoCallButton,
+  EllipsisButton,
+  MessageSeparator,
+} from "@chatscope/chat-ui-kit-react";
+import "@chatscope/chat-ui-kit-react";
+import { useState } from "react";
 
-// Integrate the SDK
-import { ZIMKitManager, Common } from '@zegocloud/zimkit-react';
-import '@zegocloud/zimkit-react/index.css';
+function ChatPage() {
+  // Set initial message input value to an empty string
+  const [messageInputValue, setMessageInputValue] = useState("");
+  return (
+    <div
+      style={{
+        height: "600px",
+        position: "relative",
+      }}
+    >
+      <MainContainer responsive>
+        <Sidebar position="left" scrollable={false}>
+          <Search placeholder="Search..." />
+          <ConversationList>
+            <Conversation
+              name="Lilly"
+              lastSenderName="Lilly"
+              info="Yes i can do it for you"
+            >
+              <Avatar src={avtr} name="Lilly" status="available" />
+            </Conversation>
 
-// The following uses the App instance as an example.
-export default class DMScreen extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            appConfig: {
-                appID: parseInt(process.env.REACT_APP_Zego_appId),       
-                serverSecret: process.env.REACT_APP_Zego_server_secret 
-            },
-            userInfo: {
-                // Your ID as a user.
-                userID: localStorage.getItem('username'),
-                // Your name as a user.
-                userName: localStorage.getItem('name'),
-                // The image you set as a user avatar must be network images. e.g., https://storage.zego.im/IMKit/avatar/avatar-0.png
-                userAvatarUrl: 'https://storage.zego.im/IMKit/avatar/avatar-0.png'
-            },
-        }
-    }
-    async componentDidMount() {
-        const zimKit = new ZIMKitManager();
-        const token = zimKit.generateKitTokenForTest(this.state.appConfig.appID, this.state.appConfig.serverSecret, this.state.userInfo.userID);
-        await zimKit.init(this.state.appConfig.appID);
-        await zimKit.connectUser(this.state.userInfo, token);
-    }
-    render() {
-        return (
-            <div ><Common></Common> </div>
-            
-        );
-    }
+            <Conversation
+              name="Joe"
+              lastSenderName="Joe"
+              info="Yes i can do it for you"
+            >
+              <Avatar src={avtr} name="Joe" status="dnd" />
+            </Conversation>
+
+            <Conversation
+              name="Emily"
+              lastSenderName="Emily"
+              info="Yes i can do it for you"
+              unreadCnt={3}
+            >
+              <Avatar src={avtr} name="Emily" status="available" />
+            </Conversation>
+
+            <Conversation
+              name="Kai"
+              lastSenderName="Kai"
+              info="Yes i can do it for you"
+              unreadDot
+            >
+              <Avatar src={avtr} name="Kai" status="unavailable" />
+            </Conversation>
+
+            <Conversation
+              name="Akane"
+              lastSenderName="Akane"
+              info="Yes i can do it for you"
+            >
+              <Avatar src={avtr} name="Akane" status="eager" />
+            </Conversation>
+
+            <Conversation
+              name="Eliot"
+              lastSenderName="Eliot"
+              info="Yes i can do it for you"
+            >
+              <Avatar src={avtr} name="Eliot" status="away" />
+            </Conversation>
+
+            <Conversation
+              name="Zoe"
+              lastSenderName="Zoe"
+              info="Yes i can do it for you"
+            >
+              <Avatar src={avtr} name="Zoe" status="dnd" />
+            </Conversation>
+
+            <Conversation
+              name="Patrik"
+              lastSenderName="Patrik"
+              info="Yes i can do it for you"
+            >
+              <Avatar src={avtr} name="Patrik" status="invisible" />
+            </Conversation>
+          </ConversationList>
+        </Sidebar>
+
+        <ChatContainer>
+          <ConversationHeader>
+            <ConversationHeader.Back />
+            <Avatar src={avtr} name="Zoe" />
+            <ConversationHeader.Content
+              userName="Zoe"
+              info="Active 10 mins ago"
+            />
+            <ConversationHeader.Actions>
+              <VoiceCallButton />
+              <VideoCallButton />
+            </ConversationHeader.Actions>
+          </ConversationHeader>
+          <MessageList>
+            <MessageSeparator content="Saturday, 30 November 2019" />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "single",
+              }}
+            >
+              <Avatar src={avtr} name="Zoe" />
+            </Message>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "single",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "first",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "normal",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "normal",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "last",
+              }}
+            >
+              <Avatar src={avtr} name="Zoe" />
+            </Message>
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "first",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "normal",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "normal",
+              }}
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Patrik",
+                direction: "outgoing",
+                position: "last",
+              }}
+            />
+
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "first",
+              }}
+              avatarSpacer
+            />
+            <Message
+              model={{
+                message: "Hello my friend",
+                sentTime: "15 mins ago",
+                sender: "Zoe",
+                direction: "incoming",
+                position: "last",
+              }}
+            >
+              <Avatar src={avtr} name="Zoe" />
+            </Message>
+          </MessageList>
+          <MessageInput
+            placeholder="Type message here"
+            value={messageInputValue}
+            onChange={(val) => setMessageInputValue(val)}
+          />
+        </ChatContainer>
+      </MainContainer>
+    </div>
+  );
 }
+export default ChatPage;
