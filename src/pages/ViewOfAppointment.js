3667580b95c09c254061f57f5529dab7ed9ffa45
Tomin Joy <tominjk007@gmail.com>
2023-08-01 14:38:13 +0530

    doctor patient mapping added

--- src/pages/ViewOfAppointment.js
+++ src/pages/ViewOfAppointment.js
@@ -9,10 +9,11 @@ import axios from "../axiosSetup";
 
 function ViewOfAppointment() {
   const [appointments, setAppointments] = useState([]);
+  const currentDate = new Date().toISOString().slice(0, 10);
 
   useEffect(() => {
     const username = localStorage.getItem("username");
-    const url = `/dappointments?username=${username}&aType=ofAppointments&date_=2023-07-01`;
+    const url = `/dappointments?username=${username}&aType=ofAppointments&date_=${currentDate}`;
 
     axios
       .get(url)
